102cc15d57a99ff11c3111c4b326a5e747bf44b4
karineko002 <hamagawa.m@gmail.com>
2021-08-09 15:08:10 +0900

    詳解JavaScript基礎文法編 #8

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 'use srtict'
-// 定数
+// 定数 const
+// 変数 let ver
 
-console.log(150 * 140);
-console.log(150 * 160);
+let price = 150;
+console.log(price * 140);
+console.log(price * 160);
 
-const price = 150;
-
+price = 170;
 console.log(price * 140);
 console.log(price * 160);
